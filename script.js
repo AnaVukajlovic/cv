ddc75e5bf0fbc0b4383b7392fe1464b36fa0486a
Ana Vukajlović <147943106+AnaVukajlovic@users.noreply.github.com>
2025-11-12 15:02:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Scroll to top
+const toTop = document.getElementById('toTop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    toTop.style.display = 'block';
+  } else {
+    toTop.style.display = 'none';
+  }
+});
+
+toTop.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
